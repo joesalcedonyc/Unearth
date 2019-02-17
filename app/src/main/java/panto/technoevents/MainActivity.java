81bf0b05b59e9e981c660e8b5d716d5bd2359260
Joseph Salcedo <mime@Josephs-MacBook-Pro.local>
2019-02-17 17:29:54 -0500

    adding background video to LoginFragment

--- app/src/main/java/panto/technoevents/MainActivity.java
+++ app/src/main/java/panto/technoevents/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import panto.technoevents.fragments.LoginFragment;
 import panto.technoevents.fragments.TechnoDjsFragment;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,10 +15,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TechnoDjsFragment technoDjsFragment = new TechnoDjsFragment();
+//        TechnoDjsFragment technoDjsFragment = new TechnoDjsFragment();
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//        fragmentTransaction.replace(R.id.main_container, technoDjsFragment);
+//        fragmentTransaction.commit();
+        LoginFragment loginFragment = new LoginFragment();
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.main_container, technoDjsFragment);
+        fragmentTransaction.replace(R.id.main_container, loginFragment);
         fragmentTransaction.commit();
     }
 }
--- app/src/main/java/panto/technoevents/fragments/LoginFragment.java
+++ app/src/main/java/panto/technoevents/fragments/LoginFragment.java
@@ -1,27 +1,31 @@
 package panto.technoevents.fragments;
 
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.VideoView;
 
 import panto.technoevents.R;
 
 public class LoginFragment extends Fragment {
-
+    private View rootView;
+    private VideoView videoView;
 
     public LoginFragment() {
-        // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        rootView = inflater.inflate(R.layout.fragment_login, container, false);
+        videoView = rootView.findViewById(R.id.login_video_background);
+        Uri uri = Uri.parse("android.resource://"+getActivity().getPackageName() + "/" + R.raw.beyer);
+        videoView.setVideoURI(uri);
+        videoView.start();
+        return rootView;
     }
-
 }
