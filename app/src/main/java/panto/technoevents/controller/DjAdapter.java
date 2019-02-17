19aff1b4d7e7d2ca268629d7709447c4e66a0044
Joseph Salcedo <mime@Josephs-MacBook-Pro.local>
2019-02-17 14:29:58 -0500

    Moving DjViewHolder class to different package

--- app/src/main/java/panto/technoevents/controller/DjAdapter.java
+++ app/src/main/java/panto/technoevents/controller/DjAdapter.java
@@ -1,20 +1,16 @@
 package panto.technoevents.controller;
 
-import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-
-import com.bumptech.glide.Glide;
 
 import java.util.List;
 
 import panto.technoevents.R;
 import panto.technoevents.model.DjModel;
-import panto.technoevents.model.view.DjViewHolder;
+import panto.technoevents.view.DjViewHolder;
 
 public class DjAdapter extends RecyclerView.Adapter<DjViewHolder> {
 
--- app/src/main/java/panto/technoevents/view/DjViewHolder.java
+++ app/src/main/java/panto/technoevents/view/DjViewHolder.java
@@ -1,14 +1,11 @@
-package panto.technoevents.model.view;
+package panto.technoevents.view;
 
-import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
 import com.squareup.picasso.Picasso;
 
 import panto.technoevents.R;
