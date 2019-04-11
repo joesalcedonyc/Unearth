f0f4a0afd4c0127fa9c32278c9aacf72321d0ef1
Joseph Salcedo <joseph.salcedo16@qmail.cuny.edu>
2019-04-11 18:21:18 -0400

    Adding splash page

--- app/src/test/java/panto/technoevents/UnitTest.java
+++ app/src/test/java/panto/technoevents/UnitTest.java
@@ -8,11 +8,6 @@ import panto.technoevents.network.DjRepository;
 
 import static org.junit.Assert.*;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 public class UnitTest {
 
 
@@ -22,5 +17,6 @@ public class UnitTest {
         DjRepository expected = DjRepository.getInstance();
 
         Assert.assertEquals(expected, DjRepository.getInstance());
+
     }
 }
